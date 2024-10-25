02e7981391f13902e9e426f761de3308b9599302
saravanakumar00 <saravanakumar364521@gmail.com>
2024-10-25 10:12:24 +0530

    fest:leaing Dynamic routing

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,9 +4,7 @@ from. import views
 
 
 urlpatterns=[
-    path('january/',views.index),
-    path('february/',views.february),
-    path('march',views.march)
+    path('<month>',views.monthly_challenges)
     
     
 
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
 
@@ -15,4 +15,19 @@ def march(re):
 
 
 
+def monthly_challenges(request,month):
+    text=None
+    if month=='january':
+        text='Hello janu'
+        
+    elif month=='february':
+        text="february"
+        
+    elif month=='march':
+        text="March"
+        
+    else:
+        return HttpResponseNotFound("This month not supported")
+    return HttpResponse(text)
+    
 
